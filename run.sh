eed606bfc92d881095a0d14b0828ef462ed01da5
Junjie Yang <yangjunjie0320@gmail.com>
2023-11-08 16:30:04 -0800

    update

---
+++ run.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+#SBATCH --partition=serial
+#SBATCH --time=120:00:00
+#SBATCH --nodes=1
+#SBATCH --cpus-per-task=28
+#SBATCH --job-name=cr2
+#SBATCH --mem=200000
+
+module purge
+module load gcc/9.2.0
+module load binutils/2.26
+module load cmake-3.6.2 
+
+export OMP_NUM_THREADS=28;
+export MKL_NUM_THREADS=28;
+export PYSCF_MAX_MEMORY=$SLURM_MEM_PER_NODE;
+echo $PYSCF_MAX_MEMORY
+
+source /home/yangjunjie/intel/oneapi/setvars.sh --force;
+export LD_LIBRARY_PATH=$MKLROOT/lib:$LD_LIBRARY_PATH
+
+export TMPDIR=/scratch/global/yangjunjie/$SLURM_JOB_NAME/$SLURM_JOB_ID/
+export PYSCF_TMPDIR=$TMPDIR
+echo TMPDIR       = $TMPDIR
+echo PYSCF_TMPDIR = $PYSCF_TMPDIR
+mkdir -p $TMPDIR
+
+export PATH=/home/yangjunjie/packages/block2/block2-preview-p0.5.2rc10/pyblock2/driver/:$PATH
+export PYTHONPATH=/home/yangjunjie/packages/pyscf/pyscf-main/;
+export PYTHONPATH=/home/yangjunjie/packages/block2/block2-preview-p0.5.2rc10/build-py-ext/:$PYTHONPATH;
+export PYTHONPATH=/home/yangjunjie/work/cas-diag-cc/:$PYTHONPATH;
+export PYTHONPATH=/home/yangjunjie/work/cas-diag-cc/work/n2/:$PYTHONPATH;
+export PYSCF_EXT_PATH=/home/yangjunjie/intel/oneapi/intelpython/python3.9/lib/python3.9/site-packages/;
+alias block2main="/home/yangjunjie/packages/block2/block2-preview-p0.5.2rc10/pyblock2/driver/block2main"
+
+python test.py
---
+++ test.py
@@ -0,0 +1,49 @@
+import pickle, os, sys
+import numpy as np
+import pyscf
+from pyscf import gto, scf, fci
+from pyscf import mcscf, mrpt
+from pyscf import lib
+
+nproc = os.environ.get("SLURM_NPROCS", 1)
+nproc = int(nproc)
+
+from pyscf import dmrgscf
+dmrgscf.settings.BLOCKEXE = os.popen("which block2main").read().strip()
+dmrgscf.settings.MPIPREFIX = 'mpirun -n %d --bind-to none' % nproc
+max_m = 2000
+
+mol = gto.Mole()
+ene_nuc, h1, eri, ncipg, weightpg, Rpg, nao, NOccSO = pickle.load(open( "FC.p", "rb" ))
+mol.nelectron = NOccSO
+ovlp = np.eye(nao)
+nso = 2 * nao
+
+# RHF
+hf_obj = scf.RHF(mol)
+hf_obj.get_hcore = lambda *args: h1
+hf_obj.get_ovlp = lambda *args: ovlp
+hf_obj._eri = pyscf.ao2mo.restore(8, eri, nao)
+hf_obj.kernel()
+hf_obj.mo_coeff = np.eye(nao)
+
+out = open("results.log","w")
+
+cas_obj = mcscf.CASSCF(hf_obj,20,28)
+cas_obj.fix_spin_(ss=0)
+cas_obj.fcisolver = dmrgscf.DMRGCI(mol, maxM=max_m)
+cas_obj.fcisolver.runtimeDir = lib.param.TMPDIR
+cas_obj.fcisolver.scratchDirectory = lib.param.TMPDIR
+cas_obj.fcisolver.threads = int(os.environ.get("OMP_NUM_THREADS", 1))
+cas_obj.fcisolver.memory = int(mol.max_memory / 1000.0 / nproc)
+cas_obj.canonicalization = True
+cas_obj.natorb = True
+cas_obj.kernel()
+print("E(CAS)=",cas_obj.e_tot + ene_nuc)
+out.write("E(CAS) = %1 12.8f\n" % (cas_obj.e_tot + ene_nuc))
+
+# nevpt
+pt_obj = mrpt.NEVPT(cas_obj)
+pt_obj.kernel()
+print("E(PT)=",pt_obj.e_corr + cas_obj.e_tot + ene_nuc)
+out.write("E(PT)  = %1 12.8f\n" % (pt_obj.e_corr + cas_obj.e_tot + ene_nuc))
