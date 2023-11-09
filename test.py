a7daecfc423930006492f271120758b79e5a9f1c
Junjie Yang <yangjunjie0320@gmail.com>
2023-11-08 17:37:51 -0800

    update

--- test.py
+++ test.py
@@ -14,7 +14,7 @@ dmrgscf.settings.MPIPREFIX = 'mpirun -n %d --bind-to none' % nproc
 max_m = 2000
 
 mol = gto.Mole()
-mol.verbose = 10
+mol.verbose = 0
 ene_nuc, h1, eri, ncipg, weightpg, Rpg, nao, NOccSO = pickle.load(open( "FC.p", "rb" ))
 mol.nelectron = NOccSO
 ovlp = np.eye(nao)
@@ -33,6 +33,7 @@ out = open("results.log","w")
 cas_obj = mcscf.CASSCF(hf_obj,20,28)
 cas_obj.fix_spin_(ss=0)
 cas_obj.fcisolver = dmrgscf.DMRGCI(mol, maxM=max_m)
+cas_obj.fcisolver.outputlevel = 5
 cas_obj.fcisolver.runtimeDir = lib.param.TMPDIR
 cas_obj.fcisolver.scratchDirectory = lib.param.TMPDIR
 cas_obj.fcisolver.threads = int(os.environ.get("OMP_NUM_THREADS", 1))
@@ -41,10 +42,10 @@ cas_obj.canonicalization = True
 cas_obj.natorb = True
 cas_obj.kernel()
 print("E(CAS)=",cas_obj.e_tot + ene_nuc)
-out.write("E(CAS) = %1 12.8f\n" % (cas_obj.e_tot + ene_nuc))
+out.write("E(CAS) = % 12.8f\n" % (cas_obj.e_tot + ene_nuc))
 
 # nevpt
 pt_obj = mrpt.NEVPT(cas_obj)
 pt_obj.kernel()
 print("E(PT)=",pt_obj.e_corr + cas_obj.e_tot + ene_nuc)
-out.write("E(PT)  = %1 12.8f\n" % (pt_obj.e_corr + cas_obj.e_tot + ene_nuc))
+out.write("E(PT)  = % 12.8f\n" % (pt_obj.e_corr + cas_obj.e_tot + ene_nuc))
